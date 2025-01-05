59a9d61f22ad39cc5261ed381b8d6fb2aa905329
RishabhPrabhu5 <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-01-05 17:52:05 -0500

    Time: 99 ms (44.44%), Space: 34 MB (7.41%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        """
+        :type s: str
+        :type shifts: List[List[int]]
+        :rtype: str
+        """
+        markers = [0]*len(s)
+        for shift in shifts:
+            if shift[2] == 0:
+                markers[shift[0]]+= -1
+                if shift[1] < len(s)-1:
+                    markers[shift[1]+1] +=1
+            else:
+                markers[shift[0]]+= 1
+                if shift[1] < len(s)-1:
+                    markers[shift[1]+1] += -1
+        total_shifts = defaultdict(int)
+        curr = 0
+        for i in range(len(s)):
+            curr +=markers[i]
+            if curr != 0:
+                total_shifts[i] = curr
+
+        to_ret = list(s)
+        for i in total_shifts:
+            to_ret[i] = chr((ord(s[i])+total_shifts[i]-97)%26 + 97)
+        return "".join(to_ret)
+
+        
