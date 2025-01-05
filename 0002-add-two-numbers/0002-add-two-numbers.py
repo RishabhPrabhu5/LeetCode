af765ca31cead4915ad1f71b93f235f09c3ae348
RishabhPrabhu5 <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-01-05 16:59:29 -0500

    Time: 23 ms (7.62%), Space: 12.6 MB (17.72%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,11 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+def linkedListToNum(l):
+    if l.next:
+        next = 10*int(linkedListToNum(l.next))
+        return l.val + next
+    return int(l.val)
 class Solution(object):
+
     def addTwoNumbers(self, l1, l2):
         """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
         """
-        
+        string = str(int(linkedListToNum(l1) + linkedListToNum(l2)))
+        print(string)
+        ret = ListNode(int(string[-1]))
+        t = ret
+        for i in range(len(string)-2, -1,-1):
+            t.next = ListNode(int(string[i]))
+            t= t.next
+        return ret
