c76dd14df4395bb643ec800b95ea3c8facee0fef
RishabhPrabhu5 <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-01-07 13:10:00 -0500

    Time: 3 ms (79.46%), Space: 12.4 MB (20.54%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -5,10 +5,8 @@ class Solution(object):
         :rtype: List[str]
         """
         substrings = []
-        for i in range(len(words)):
-            for j in range(len(words)):
-                word = words[i]
-                second = words[j]
+        for word in words:
+            for second in words:
                 if second != word and word in second:
                     substrings.append(word)
                     break
