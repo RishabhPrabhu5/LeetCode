f63e241507b91ccf0ed0f549be6c7bb3886bb25e
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-01-11 14:50:24 -0500

    Time: 44 ms (93.75%), Space: 13 MB (37.5%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canConstruct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        if k>len(s):
+            return False
+        mp = defaultdict(int)
+        for l in s:
+            mp[l]+=1
+        odd_count = 0
+        for l in mp:
+            if mp[l]%2 == 1:
+                odd_count+=1
+            if odd_count > k:
+                return False
+        return True
+        
