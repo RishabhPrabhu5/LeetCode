247791cf9c9dc8ea0027626873bfb3e3778a71f5
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-02-09 12:24:47 -0500

    Time: 95 ms (57.14%), Space: 27.5 MB (14.29%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -10,7 +10,6 @@ class Solution(object):
             diff = i - nums[i]
             count += duplicates[diff]
             duplicates[diff]+=1
-        print(duplicates)
 
         return (len(nums) * (len(nums)-1))/2 - count
 
