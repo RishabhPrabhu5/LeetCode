9426c8d06fbc8d9a05a79dae21636f1c57438e0e
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-05-21 14:56:48 -0400

    Time: 435 ms (92%), Space: 20.8 MB (37.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
 
         for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1] or nums[i]>0 :
                 continue
             
             j = i + 1
@@ -29,5 +25,3 @@ class Solution(object):
                         j += 1
         
         return res
-
-        
