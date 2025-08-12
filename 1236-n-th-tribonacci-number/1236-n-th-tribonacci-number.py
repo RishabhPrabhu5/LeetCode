356c14f1628a9241cb4c4f9c6bbd5619d67c5519
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-08-12 16:40:46 -0500

    Time: 0 ms (100%), Space: 12.6 MB (12.51%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = {}
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        return dp[n]
