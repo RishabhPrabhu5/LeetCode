9574c90073481d907a73acf93db1abd10ee0d243
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-08-12 20:25:29 -0500

    Time: 0 ms (100%), Space: 14.2 MB (20.73%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -4,28 +4,42 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        mp = defaultdict(int)
+        # mp = defaultdict(int)
+        # for num in nums:
+        #     mp[num] +=1
+        # arr = mp.keys()
+
+        # dp = {}
+        # dp[0] = arr[0] * mp[arr[0]]
+        # if len(arr) < 2:
+        #     return dp[0]
+
+        # if arr[0] < arr[1] -1:
+        #     dp[1] = dp[0] + arr[1]*mp[arr[1]]
+        # else:
+        #     dp[1] = max(arr[0] * mp[arr[0]], arr[1] * mp[arr[1]])
+
+
+        # for i in range(2, len(arr)):
+        #     if arr[i-1] < arr[i] -1:
+        #         dp[i] = dp[i-1] + arr[i]*mp[arr[i]]
+        #     else:
+        #         dp[i] = max(arr[i]*mp[arr[i]] + dp[i-2], dp[i-1])
+
+        # return dp[len(arr)-1]
+
+        upperLimit = max(nums) + 1 
+        store = [0] * upperLimit
+
         for num in nums:
-            mp[num] +=1
-        arr = mp.keys()
+            store[num] += num
 
-        dp = {}
-        dp[0] = arr[0] * mp[arr[0]]
-        if len(arr) < 2:
-            return dp[0]
+        dp = [0] * upperLimit
 
-        if arr[0] < arr[1] -1:
-            dp[1] = dp[0] + arr[1]*mp[arr[1]]
-        else:
-            dp[1] = max(arr[0] * mp[arr[0]], arr[1] * mp[arr[1]])
+        dp[1] = 1 * store[1]
+        for i in range(2, upperLimit):
+            dp[i] = max(dp[i - 2] + store[i], dp[i - 1])
 
-
-        for i in range(2, len(arr)):
-            if arr[i-1] < arr[i] -1:
-                dp[i] = dp[i-1] + arr[i]*mp[arr[i]]
-            else:
-                dp[i] = max(arr[i]*mp[arr[i]] + dp[i-2], dp[i-1])
-
-        return dp[len(arr)-1]
+        return dp[-1]
  
         
