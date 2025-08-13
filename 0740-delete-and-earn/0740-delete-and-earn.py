828cfe895932b0e501ddcdffbdd01d94cbd9f8d4
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-08-12 20:26:58 -0500

    Time: 4 ms (92.44%), Space: 14.1 MB (43.7%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -36,7 +36,7 @@ class Solution(object):
 
         dp = [0] * upperLimit
 
-        dp[1] = 1 * store[1]
+        dp[1] = store[1]
         for i in range(2, upperLimit):
             dp[i] = max(dp[i - 2] + store[i], dp[i - 1])
 
