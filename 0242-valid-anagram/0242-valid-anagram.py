3545a51a90d1516c653219c9d9471d2a957e4bff
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-12-16 12:22:35 -0500

    Time: 22 ms (55.68%), Space: 14.5 MB (9.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,5 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
         
