585c2b1d7fc1a75a874d0668e892e1f52ed0bdbc
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-12-16 11:44:34 -0500

    Time: 12 ms (78.02%), Space: 13.3 MB (44.52%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        cleaned_string = "".join(c for c in s if c.isalnum())
+        s =  cleaned_string.lower()
+        print(s)
+        l = 0
+        r = len(s) -1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+        
