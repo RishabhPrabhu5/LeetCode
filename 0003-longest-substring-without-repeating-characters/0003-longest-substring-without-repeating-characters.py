a36e288622ad6ccd842ecc15bd1eb1d856b14207
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2026-01-09 17:53:25 -0500

    Time: 16 ms (83.63%), Space: 13.1 MB (57.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -12,11 +12,9 @@ class Solution(object):
         for i in range(len(s)):
             c = s[i]
             if c in last_idx and last_idx[c] >= start:
-                #  print(start, i)
                 max_len = max(max_len, i - start)
                 start = last_idx[c]+1
             last_idx[c] = i
-            # print(last_idx)
             
         max_len = max(max_len, i - start + 1)
 
