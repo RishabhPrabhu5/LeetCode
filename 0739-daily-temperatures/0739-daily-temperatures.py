c4af832c4a0da611a6d4c8e371a78f74541acf2c
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2026-02-14 12:28:11 -0500

    Time: 139 ms (96.95%), Space: 24.5 MB (90.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        answer = [0]*len(temperatures)
-        stack = []
-        for i in range(len(temperatures)):
-            while len(stack)> 0 and temperatures[i] > stack[-1][0]:
-                temp, day = stack.pop()
+        answer = [0] * len(temperatures)
+        stack = []  # will store indices only
+        
+        for i, temp in enumerate(temperatures):
+            # while current temperature is warmer than the temperature
+            # at the index on top of the stack
+            while stack and temp > temperatures[stack[-1]]:
+                day = stack.pop()
                 answer[day] = i - day
-
-            stack.append((temperatures[i], i))
+            
+            stack.append(i)
+        
         return answer
 
 
