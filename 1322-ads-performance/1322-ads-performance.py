38925c200f5b6f7abcf8c961260e0f686080f78e
Rishabh Prabhu <103843699+RishabhPrabhu5@users.noreply.github.com>
2026-04-20 18:26:54 -0400

    Time: 598 ms (11.85%), Space: 68.3 MB (77.17%) - LeetHub

---
+++ 1322-ads-performance/1322-ads-performance.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def ads_performance(ads: pd.DataFrame) -> pd.DataFrame:
+    ctr = ads.groupby('ad_id')['action'].apply(lambda x: round(
+        (100 * sum(x == "Clicked")/(sum(x == "Clicked") + sum(x == "Viewed"))) if 
+        (sum(x == "Clicked") + sum(x == "Viewed")) > 0 else 0.00
+    , 2)).reset_index()
+
+    print(ctr)
+    ctr.columns = ['ad_id', 'ctr']
+    
+
+    return ctr.sort_values(by = ['ctr', 'ad_id'], ascending = [False, True])
+    
